a6e84ab71e6c9a3ed77a64c55afcd0fd5b704305
rinaresa <rahinamoro58@gmail.com>
2025-05-23 22:27:04 +0000

    Fix filename typo and add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+#define the number
+number1 = 10
+number2 = 5
+
+#perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+
+#print results
+print(f"Addition of (number1) and (number2) is {addition})
+print(f"Subtraction of (number1) and (number2) is {subtraction})
+print(f"Multiplication of (number1) and (number2) is {multiplication})
